b51e71d192216b0d56b1a6c764500275c24fbcca
Halil İbrahim <53411103+hidirektor@users.noreply.github.com>
2020-12-18 19:57:25 +0300

    Added !
    • Added result image,
    • Updated all code,
    • Updated readme file.

--- Cloud_Lamp.ino
+++ Cloud_Lamp.ino
@@ -2,23 +2,24 @@
 #include <FastLED.h>
 
 #define LED_SAYISI 60
-#define DATA_PIN 6
+#define RGB_DATA 6
 
 
-enum Mode { CLOUD,ACID,OFF,ON,RED,GREEN,BLUE,FADE};
-Mode mode = CLOUD;  
-Mode lastMode = CLOUD;
+enum Modlar { 
+  BULUT,ACID,OFF,ON,RED,GREEN,BLUE,FADE 
+  };
+  
+Modlar mod = BULUT;  
+Modlar sonMod = BULUT;
 
-#define MIC_PIN   0
+#define Mikrofon   0
 #define DC_OFFSET  0
 #define NOISE     10
 #define SAMPLES   10
-byte
-  volCount  = 0;
-int
-  vol[SAMPLES];
-int      n, total = 30;
-float average = 0;
+byte volCount  = 0;
+int vol[SAMPLES];
+int n, toplam = 30;
+float ortalama = 0;
   
 int fade_h;
 int fade_direction = 1;
@@ -27,7 +28,7 @@ int fade_direction = 1;
 CRGB leds[LED_SAYISI];
 
 void setup() {
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, LED_SAYISI); 
+  FastLED.addLeds<WS2812B, RGB_DATA, GRB>(leds, LED_SAYISI); 
   memset(vol, 15, sizeof(vol));
   Serial.begin(115200);
   Wire.begin(9);
@@ -40,59 +41,80 @@ void receiveEvent(int bytes) {
   while(Wire.available())
    { 
       unsigned int received = Wire.read();
-      Serial.print("Receiving IR hex: ");
+      Serial.print("HEX: ");
       Serial.println(received,HEX);
-      lastMode = mode;
+      sonMod = mod;
       switch(received){
         case 0x3F:
-          mode = ON; break;
+          mod = ON; break;
         case 0xBF:
-          mode = OFF; break;
+          mod = OFF; break;
         case 0x2F:
-          mode = CLOUD; break;
+          mod = BULUT; break;
         case 0xF:
-          mode = ACID; break;
+          mod = ACID; break;
         case 0x37:
-          mode = FADE; break;
+          mod = FADE; break;
         case 0x9F:
-          mode = BLUE; break;
+          mod = BLUE; break;
         case 0x5F:
-          mode = GREEN; break;
+          mod = GREEN; break;
         case 0xDF:
-          mode = RED; break;
+          mod = RED; break;
         
       }
    }
 
 }
  
-void loop() { 
-  switch(mode){
-    case CLOUD: detect_thunder();reset();break;
-    case ACID: acid_cloud();reset();break;
-    case OFF:reset();break;
-    case ON: constant_lightning();reset();break;
-    case RED: single_colour(0);break;
-    case BLUE: single_colour(160);break;
-    case GREEN: single_colour(96);break;
-    case FADE: colour_fade();break;
-    default: detect_thunder(); reset();break; 
+void loop() {
+  switch(mod){
+    case BULUT:
+      simsekAlgilama();
+      reset();
+      break;
+    case ACID:
+      bulut();
+      reset();
+      break;
+    case OFF:
+      reset();
+      break;
+    case ON:
+      sabitSimsek();
+      reset();
+      break;
+    case RED:
+      tekRenk(0);
+      break;
+    case BLUE:
+      tekRenk(160);
+      break;
+    case GREEN:
+      tekRenk(96);
+      break;
+    case FADE:
+      renkSolma();
+      break;
+    default:
+      simsekAlgilama(); 
+      reset();
+      break; 
   }
-  
 }
 
-void single_colour(int H){
+void tekRenk(int H){
   for (int i=0;i<LED_SAYISI;i++){
     leds[i] = CHSV( H, 255, 255);
   }
-  if(lastMode != mode){
+  if(sonMod != mod){
     FastLED.show(); 
-    lastMode = mode;
+    sonMod = mod;
   }
   delay(50);
 }
 
-void colour_fade(){
+void renkSolma(){
   for (int i=0;i<LED_SAYISI;i++){
     leds[i] = CHSV( fade_h, 255, 255);
   }
@@ -110,16 +132,16 @@ void colour_fade(){
 
 
 
-void detect_thunder() {
-  n   = analogRead(MIC_PIN);
+void simsekAlgilama() {
+  n   = analogRead(Mikrofon);
   n   = abs(n - 512 - DC_OFFSET);
   n   = (n <= NOISE) ? 0 : (n - NOISE);
   vol[volCount] = n;
   if(++volCount >= SAMPLES) volCount = 0;
  
-  total = 0;
+  toplam = 0;
   for(int i=0; i<SAMPLES; i++) {
-    total += vol[i];
+    toplam += vol[i];
   }
   
   // DEBUG BLOCK.
@@ -143,32 +165,23 @@ void detect_thunder() {
   Serial.println(n);
   */
   
-  average = (total/SAMPLES)+2;
-  if(n>average){
-    Serial.println("TRIGGERED");
+  ortalama = (toplam/SAMPLES)+2;
+  if(n>ortalama){
     reset();
-     
     switch(random(1,3)){
-       //switch(3){
-  
       case 1:
-        thunderburst();
+        gokGurultusu();
         delay(random(10,500));
-         Serial.println("Thunderburst");
         break;
        
       case 2:
-        rolling();
-        Serial.println("Rolling");
+        gurleme();
         break;
         
       case 3:
-        crack();
+        catlak();
         delay(random(50,250));
-        Serial.println("Crack");
-        break;
-        
-      
+        break;     
     }
   }
 }
@@ -181,7 +194,7 @@ void reset(){
    
 }
 
-void acid_cloud(){
+void bulut(){
     for(int i=0;i<LED_SAYISI;i++){
       if(random(0,100)>90){
         leds[i] = CHSV( random(0,255), 255, 255); 
@@ -196,12 +209,11 @@ void acid_cloud(){
     reset();
 }
 
-void rolling(){
+void gurleme(){
   for(int r=0;r<random(2,10);r++){
     for(int i=0;i<LED_SAYISI;i++){
       if(random(0,100)>90){
-        leds[i] = CHSV( 0, 0, 255); 
-
+        leds[i] = CHSV( 0, 0, 255);
       }
       else{
         leds[i] = CHSV(0,0,0);
@@ -214,7 +226,7 @@ void rolling(){
   }
 }
 
-void crack(){
+void catlak(){
    for(int i=0;i<LED_SAYISI;i++) {
       leds[i] = CHSV( 0, 0, 255);  
    }
@@ -223,7 +235,7 @@ void crack(){
    reset();
 }
 
-void thunderburst(){
+void gokGurultusu(){
 
   int rs1 = random(0,LED_SAYISI/2);
   int rl1 = random(10,20);
@@ -251,23 +263,20 @@ void thunderburst(){
   
 }
  
-void constant_lightning(){
+void sabitSimsek(){
   switch(random(1,10)){
    case 1:
-        thunderburst();
+        gokGurultusu();
         delay(random(10,500));
-         Serial.println("Thunderburst");
         break;
        
       case 2:
-        rolling();
-        Serial.println("Rolling");
+        gurleme();
         break;
         
       case 3:
-        crack();
+        catlak();
         delay(random(50,250));
-        Serial.println("Crack");
         break;
         
     
